2e1d959c5619af7adf85e7dcc8eb6134e6823bae
Abhinav shyju <abhinavshyjupc@gmail.com>
2024-07-09 22:28:28 +0530

    test 2

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-require("./bot");
+const mineflayer = require("mineflayer");
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: "51mple.aternos.me",
+    port: 43008,
+    username: "Bot",
+  });
+
+  bot.on("login", () => {
+    console.log("Bot has logged in");
+    bot.chat("Hello, world!");
+  });
+
+  bot.on("chat", (username, message) => {
+    if (username === bot.username) return;
+    bot.chat(`You said: ${message}`);
+  });
+
+  bot.on("error", (err) => console.log(err));
+  bot.on("end", () => {
+    console.log("Bot has disconnected");
+    setTimeout(createBot, 5000);
+  });
+}
 
 app.get("/", (req, res) => {
   try {
-    require("./bot");
+    createBot();
     res.send("Minecraft Bot is running");
   } catch (error) {
     res.send(error);
